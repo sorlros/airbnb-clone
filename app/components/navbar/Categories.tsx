baa1ed813bcfae4725db487018619098164b14d8
sorlros <sorlros2047@gmail.com>
2023-09-12 15:19:32 +0900

    Categories UI is done

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import Container from "../Container";
-import { GiWindmill } from "react-icons/gi";
-import { TbBeach } from "react-icons/tb";
+import {
+	GiBoatFishing,
+	GiCactus,
+	GiCastle,
+	GiCaveEntrance,
+	GiForestCamp,
+	GiIsland,
+	GiWindmill,
+	GiBarn,
+} from "react-icons/gi";
+import { IoDiamond } from "react-icons/io5";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import { MdOutlineVilla } from "react-icons/md";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
 
@@ -21,7 +33,67 @@ export const categories = [
 	{
 		label: "Modern",
 		icon: MdOutlineVilla,
-		description: "THis property is modern",
+		description: "This property is modern",
+	},
+	{
+		label: "Countryside",
+		icon: TbMountain,
+		description: "This property is in the countryside",
+	},
+	{
+		label: "Pools",
+		icon: TbPool,
+		description: "This property has a pool",
+	},
+	{
+		label: "Islands",
+		icon: GiIsland,
+		description: "This property is on an island",
+	},
+	{
+		label: "Lake",
+		icon: GiBoatFishing,
+		description: "This property is close to a lake",
+	},
+	{
+		label: "Skiing",
+		icon: FaSkiing,
+		description: "This property is has skiing activities",
+	},
+	{
+		label: "Castles",
+		icon: GiCastle,
+		description: "This property is in a castle",
+	},
+	{
+		label: "Camping",
+		icon: GiForestCamp,
+		description: "This property is has camping activities",
+	},
+	{
+		label: "Arctic",
+		icon: BsSnow,
+		description: "This property is in arctic",
+	},
+	{
+		label: "Cave",
+		icon: GiCaveEntrance,
+		description: "This property is in a cave",
+	},
+	{
+		label: "Desert",
+		icon: GiCactus,
+		description: "This property is in the desert ",
+	},
+	{
+		label: "Barns",
+		icon: GiBarn,
+		description: "This property is in the barn",
+	},
+	{
+		label: "Lux",
+		icon: IoDiamond,
+		description: "This property is luxurious",
 	},
 ];
 
@@ -29,6 +101,7 @@ const Categories = () => {
 	const params = useSearchParams();
 	const category = params?.get("category");
 	const pathname = usePathname();
+
 	const isMainPage = pathname === "/";
 
 	if (!isMainPage) {
